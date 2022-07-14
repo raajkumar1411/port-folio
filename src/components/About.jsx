08277368fa419ef7358bcf76cf72f81bbebd4a9a
raajkumar <raajkumar1999@gmail.com>
2022-07-14 08:53:34 +0530

    added

---
+++ src/components/About.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function About() {
+  return (
+
+    <div  name='about'className='w-full h-full bg-gradient-to-b from-gray-800 to-black text-white py-10 '>
+        <div >
+            <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full  '>
+                <div className='pb-6'>
+                <p className=' font-bold text-4xl  inline border-b-4 border-gray-500'>About</p>
+                </div>
+                    
+            </div>
+            <div className='ml-12 mx-5 '>
+            <p className='text-xl mt-10'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident perferendis, asperiores quas at in quo laboriosam, unde molestiae cupiditate dicta, autem nisi illum modi. Aspernatur, deleniti soluta! Dolorem cupiditate quibusdam modi nemo cumque eos praesentium suscipit magnam esse veritatis. Quis dignissimos magnam quasi fuga esse cum illo facilis adipisci odit!</p>      
+         <br />
+         <p className='text-xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident perferendis, asperiores quas at in quo laboriosam, unde molestiae cupiditate dicta, autem nisi illum modi. Aspernatur, deleniti soluta! Dolorem cupiditate quibusdam modi nemo cumque eos praesentium suscipit magnam esse veritatis. Quis dignissimos magnam quasi fuga esse cum illo facilis adipisci odit!</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default About
---
+++ src/components/Home.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import pic from '../assets/pic.jpg';
+import {Link} from 'react-scroll';
+import {MdOutlineKeyboardArrowRight} from 'react-icons/md';
+
+
+const Home = () => {
+  return (
+    <div name="home" className=' h-screen w-full bg-gradient-to-b from-black to-gray-800 pb-11'>
+        
+        <div className='max-w-screen-lg  mx-5  flex flex-col items-center justify-center h-full  text-white px-4 md:flex-row'>
+            <div className='flex flex-col justify-center h-full  mt-4 mx-5  ' >
+                <h2 className='text-4xl sm:text-7xl font-bold mt-5'> I'm a Frontend Developer</h2>
+                <p className='text-gray-500 py-6'>Currently, I love to work on web application using techologies like  HTML,CSS,JS,React,Tailwind,Node Js,Express Js and MongoDb.
+
+                </p>
+                <div  >
+<Link to='portfolio' smooth duration={700} className='text-whtie w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer'> portfolio 
+<span className='hover:rotate-90 duration-500'>
+    <MdOutlineKeyboardArrowRight size={25} className='ml-1'/>
+    </span> 
+    </Link>
+                </div>
+            </div>
+            <div className=' mt-2' >
+        <img src={pic} alt="raajkumar" className='rounded-2xl w-auto h-56 overflow-hidden md:w-full   hover:scale-110' />
+
+            </div>
+        </div>
+    </div>
+
+  )
+}
+
+export default Home
